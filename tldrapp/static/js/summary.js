c6d19c0c8d07a2b8f0e0dd3a67dbd31b095cd6ea
jaijuneja <jai.juneja@gmail.com>
2015-03-11 03:17:23 +0000

    made summary buttons more responsive

--- tldrapp/static/js/summary.js
+++ tldrapp/static/js/summary.js
@@ -14,7 +14,8 @@ var menu_elements = document.querySelectorAll('.summary-buttons button'),
     menu_length = menu_elements.length;
 for (var i = 0; i < menu_length; i++) {
     menu_elements[i].addEventListener('click', function (e) {
-        var target = document.querySelector('.summary-container>.' + e.target.classList[0]); // clicked element
+        var target_button = e.target.closest('button');
+        var target = document.querySelector('.summary-container>.' + target_button.classList[0]); // clicked element
         Array.prototype.filter.call(target.parentNode.children, function (siblings) {
             siblings.style.display = 'none'; // hide sibling elements
         });
